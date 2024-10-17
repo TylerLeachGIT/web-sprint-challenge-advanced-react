98a863821ec3d05b7e3129a01398eec2f9b44043
TylerLeachGITgit config --global user.email tylerleach2918@gmail.com <tylerleachgit@gmail.com>
2024-10-17 13:07:57 -0700

    showing 2/2 60passed

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import AppFunctional from './AppFunctional';
 
 // Write your tests here
